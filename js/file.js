b24f0d3d925632bb6d279bc9bb41b8303f17defd
acesmndr <acesmndr@gmail.com>
2014-12-09 19:54:16 +0545

    WEBSQL functions added and Download Test

--- js/file.js
+++ js/file.js
@@ -1,17 +1,45 @@
 var db = openDatabase('studentDatabase','1.0','Attendance Register',3*1024*1024);
 db.transaction(function(transaction){
-		transaction.executeSql('CREATE TABLE IF NOT EXISTS tableOfClasses(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,cName VARCHAR(20) NOT NULL)');
+		transaction.executeSql('CREATE TABLE IF NOT EXISTS classIndex(id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,nameOfClass TEXT NOT NULL)',[],initializeDatabase);
 });
+//});
+function initializeDatabase(){
 db.transaction(function(transaction){
-		transaction.executeSql('INSERT INTO tableOfClasses(id,cName) VALUES (1,"English")');
-		transaction.executeSql('INSERT INTO tableOfClasses(id,cName) VALUES (2,"Nepali")');
-		transaction.executeSql('select * from tableOfClasses',[],function(transaction, results){
+		transaction.executeSql('INSERT INTO classIndex(id,nameOfClass) VALUES(1,"BCT")');
+		transaction.executeSql('INSERT INTO classIndex(nameOfClass) VALUES("BEX")');
+		transaction.executeSql('INSERT INTO classIndex(nameOfClass) VALUES("ARCH")');
+		});
+};
+(function sidebarClassesLoad(){
+db.transaction(function(transaction){
+transaction.executeSql('select * from classIndex',[],function(transaction, results){
 		     					 for (var j=0; j<results.rows.length; j++) {
-		      							  	var row = results.rows.item(j);
-			        						$("#addGroup").prepend($(document.createElement("div")).html(row[cName]).addClass("addGroup"));
+										var row = results.rows.item(j);
+			        						$("#classList").prepend($(document.createElement("div")).html(row.nameOfClass).addClass("addGroup"));
 						 			}
 								});
+	});
+})();
+
+function todalNoOfDays(){
+db.transaction(function(transaction){
+		transaction.executeSql('SELECT COUNT(*) FROM ? WHERE studentId=?',[nameOfTable,"68001"],function(transaction,results){
+		return results.row.items(0);	
 		});
+	});
+} 
+function presentDays(studentId){
+db.transaction(function(transaction){
+		transaction.executeSql('SELECT COUNT(*) FROM ? WHERE studentId=? AND attendance=?',[nameOfTable,studentId,attendance],function(transaction,results){
+		return results.row.items(0);		
+			});
+		});
+	}
+function updateTable(nameOfTable){
+db.transaction(function(transaction){
+		transaction.executeSql('UPDATE '+nameOfTable+' SET attendance=1 WHERE studentId='+studentId+' AND date='+todaysDate);
+	});
+};
 function createTable(nameOfTable){
 	db.transaction(
          function (transaction) {
@@ -38,48 +66,37 @@ db.transaction(function (transaction) {
 	
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
+   window.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, fileSystemSuccess, fileSystemFail);
 }
-
-var csvContent = "data:text/csv;charset=utf-8,";
-csvContent+="aces,rab,todd,roben\naces,dfg,eret,dfgdfg";
-var encodedUri = encodeURI(csvContent);
-/*var downloadLink = document.createElement("a");  
-downloadLink.href = encodedUri;
-downloadLink.download = "final.csv";   
-
-document.body.appendChild(downloadLink);
-downloadLink.click();                   //for exporting the csv file
-document.body.removeChild(downloadLink);
-function downloadFile(){  
-       var fileTransfer = new FileTransfer();*/
+function fileSystemFail(){
+	alert("File System API failed");
+	}
+function dirReady(entry) {
+		window.appRootDir = entry;
+		console.log(JSON.stringify(window.appRootDir));
+	}
 function fileSystemSuccess(fileSystem) {
-    var directoryEntry = fileSystem.root; // to get root path to directory
-    directoryEntry.getDirectory("<folder_name>", {create: true, exclusive: false}, onDirectorySuccess, onDirectoryFail);
-    var rootdir = fileSystem.root;
-    var fp = rootdir.fullPath;
-    fp = fp+"/aces.csv";
-	fileTransfer.download(
+	var csvContent = "data:text/csv;charset=utf-8,";
+	csvContent+="aces,rab,todd,roben\naces,dfg,eret,dfgdfg";
+	var encodedUri = encodeURI(csvContent);
+	var directoryEntry = fileSystem.root; // to get root path to directory
+    	fileSystem.root.getDirectory(window.appRootDirName, {create : true,exclusive : false}, dirReady, fileSystemFail);
+	/*var rootdir = fileSystem.root;
+    		alert(rootdir);	
+    	var fp = rootdir.fullPath;
+    	fp = fp+"/attendanceRegister.csv";
+    		alert(fp);*/
+	ft=new FileTransfer();	
+	ft.download(
     		encodedUri,
-    		fp,
+    		window.appRootDir.fullPath+"/ar.csv",
     		function(entry) {
-        		console.log("download complete: " + entry.fullPath);
+        		alert("download complete: " + entry.fullPath);
     		},
     		function(error) {
         		alert("Download Failed");
     		}
 );
 }
-  // downloadFile();  
-
-
-
-
-
-
-
-
-
-
-
-//});
+  
